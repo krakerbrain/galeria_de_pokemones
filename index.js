76e785a3bd70dc0a3ce060fa2956355b1c9fd5f1
krakerbrain <marioplantabaja@gmail.com>
2021-09-01 22:33:09 -0400

    Desafìo completado

---
+++ index.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const http = require("http");
+
+async function pokemonesGet() {
+  const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=75");
+  return data.results;
+}
+async function pokemonesGet2() {
+  const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=75&offset=150");
+  return data.results;
+}
+
+async function getFullData(name) {
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  return data;
+}
+
+http
+  .createServer((req, res) => {
+    let pokemonesPromesas = [];
+    let listaPokemones = new Array();
+
+    pokemonesGet().then((results) => {
+      results.forEach((p) => {
+        let pokemonName = p.name;
+        pokemonesPromesas.push(getFullData(pokemonName));
+      });
+      pokemonesGet2().then((results) => {
+        results.forEach((p) => {
+          let pokemonName = p.name;
+          pokemonesPromesas.push(getFullData(pokemonName));
+        });
+        Promise.all(pokemonesPromesas).then((data) => {
+          data.forEach((p) => {
+            listaPokemones.push({ nombre: p.name, img: p.sprites.front_default });
+          });
+
+          const url = req.url;
+          if (url == "/pokemones") {
+            res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
+            let yeison = JSON.stringify(listaPokemones);
+            res.end(yeison);
+          }
+        });
+      });
+    });
+  })
+  .listen(3000, () => console.log("Escuchando el puerto 3000"));
