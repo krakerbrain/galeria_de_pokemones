d17e11a1e159a1a739f691fab73dafe6e67b736f
krakerbrain <marioplantabaja@gmail.com>
2021-09-01 23:45:42 -0400

    Se agregó ruta de acceso a /galeria para que se vieran las imagenes y se dejó la ruta /pokemon para ver el JSON

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const http = require("http");
+const fs = require("fs");
 
 async function pokemonesGet() {
   const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=75");
@@ -18,31 +19,29 @@ async function getFullData(name) {
 http
   .createServer((req, res) => {
     let pokemonesPromesas = [];
-    let listaPokemones = new Array();
+    let listaPokemones = [];
 
-    pokemonesGet().then((results) => {
-      results.forEach((p) => {
-        let pokemonName = p.name;
-        pokemonesPromesas.push(getFullData(pokemonName));
-      });
-      pokemonesGet2().then((results) => {
+    if (req.url == "/pokemones") {
+      pokemonesGet().then((results) => {
         results.forEach((p) => {
           let pokemonName = p.name;
           pokemonesPromesas.push(getFullData(pokemonName));
         });
-        Promise.all(pokemonesPromesas).then((data) => {
-          data.forEach((p) => {
-            listaPokemones.push({ nombre: p.name, img: p.sprites.front_default });
+        pokemonesGet2().then((results) => {
+          results.forEach((p) => {
+            let pokemonName = p.name;
+            pokemonesPromesas.push(getFullData(pokemonName));
           });
-
-          const url = req.url;
-          if (url == "/pokemones") {
+          Promise.all(pokemonesPromesas).then((data) => {
+            data.forEach((p) => {
+              listaPokemones.push({ nombre: p.name, img: p.sprites.front_default });
+            });
             res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
             let yeison = JSON.stringify(listaPokemones);
             res.end(yeison);
-          }
+          });
         });
       });
-    });
+    }
   })
   .listen(3000, () => console.log("Escuchando el puerto 3000"));
